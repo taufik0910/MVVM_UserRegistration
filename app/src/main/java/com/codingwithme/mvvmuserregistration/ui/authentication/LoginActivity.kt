83315060322cd811cb7a8be1534d3a0cce06aae4
taufik0910 <taufik.cradle@gmail.com>
2023-01-17 20:50:18 +0700

    firt commit

--- app/src/main/java/com/codingwithme/mvvmuserregistration/ui/authentication/LoginActivity.kt
+++ app/src/main/java/com/codingwithme/mvvmuserregistration/ui/authentication/LoginActivity.kt
@@ -4,19 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.codingwithme.mvvmuserregistration.R
 import com.codingwithme.mvvmuserregistration.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() , AuthListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var dataBinding:ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
 
-        var viewModel = ViewModelProvider(this).get(AuthenticationViewModel::class.java)
+            val dataBinding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val viewModel = ViewModelProvider(this)[AuthenticationViewModel::class.java]
 
         dataBinding.loginViewModel = viewModel
 
